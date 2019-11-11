b49a9a1a40922c80a3307935bac362056ad400d4
Denis Korolev <denkorolev90@gmail.com>
2019-11-11 17:00:59 +0300

    Main features

---
+++ eng/vocab/src/main/java/com/dkorolev/edu/eng/vocab/Vocab.java
@@ -0,0 +1,82 @@
+package com.dkorolev.edu.eng.vocab;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Stream;
+
+public class Vocab {
+
+    public static final String PATTERN = "[0-9]| |<|>|--|:|,|\\.|\\/|\\n|\\?";
+
+    public static void main(String[] args) {
+	new Vocab().test();
+    }
+
+    public void test() {
+	System.out.println("Working Directory = " + System.getProperty("user.dir"));
+	String s = fileToString("resource/Blade Runner (1982).srt");
+	s = doVocabStuff(s);
+	System.out.println(s);
+    }
+
+    public Vocab() {
+    }
+
+    private boolean contains(Set<String> strings, String s) {
+//	for (String string : strings) {
+//	    if (string.toUpperCase().equals(s.toUpperCase())) {
+//		return true;
+//	    }
+//	}
+//	return false;
+	return strings.contains(s);
+    }
+
+    public String doVocabStuff(String text) {
+	String[] splitted = text.split(PATTERN);
+	Map<Word, Integer> wordMap = new HashMap<>();
+
+	for (String word : splitted) {
+	    Word wordObj = new Word(word);
+	    if (wordMap.containsKey(wordObj)) {
+		wordMap.put(wordObj, wordMap.get(wordObj) + 1);
+	    } else {
+		wordMap.put(wordObj, 1);
+	    }
+	}
+
+	List<Word> words = new ArrayList<>(wordMap.keySet());
+	words.sort(new Comparator<Word>() {
+	    public int compare(Word o1, Word o2) {
+		return Integer.compare(wordMap.get(o2), wordMap.get(o1));
+	    };
+	});
+
+	StringBuilder sb = new StringBuilder();
+	for (Word word : words) {
+	    sb
+		    // .append(wordMap.get(word)).append(" ")
+		    .append(word).append("\n");
+	}
+
+	return sb.toString();
+    }
+
+    public String fileToString(String path) {
+	StringBuilder contentBuilder = new StringBuilder();
+	try (Stream<String> stream = Files.lines(Paths.get(path), StandardCharsets.UTF_8)) {
+	    stream.forEach(s -> contentBuilder.append(s).append("\n"));
+	} catch (IOException e) {
+	    e.printStackTrace();
+	}
+	return contentBuilder.toString();
+    }
+}
---
+++ eng/vocab/src/main/java/com/dkorolev/edu/eng/vocab/VocabEntry.java
@@ -0,0 +1,6 @@
+package com.dkorolev.edu.eng.vocab;
+
+public class VocabEntry {
+    String word;
+    int count;
+}
