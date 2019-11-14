f2b370d8136e82bdbdc03cf44a99462f072af898
Denis Korolev <denkorolev90@gmail.com>
2019-11-14 18:58:22 +0300

    rick_m

--- eng/vocab/src/main/java/com/dkorolev/edu/eng/vocab/Vocab.java
+++ eng/vocab/src/main/java/com/dkorolev/edu/eng/vocab/Vocab.java
@@ -1,16 +1,11 @@
 package com.dkorolev.edu.eng.vocab;
 
-import java.io.IOException;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
-import java.util.stream.Stream;
 
 public class Vocab {
 
