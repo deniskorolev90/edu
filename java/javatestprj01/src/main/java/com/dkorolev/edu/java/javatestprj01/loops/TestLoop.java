b165c44980bee8b4900b271a4884fb9b78880962
Denis Korolev <denkorolev90@gmail.com>
2020-02-06 18:41:44 +0300

    loops

---
+++ java/javatestprj01/src/main/java/com/dkorolev/edu/java/javatestprj01/loops/TestLoop.java
@@ -0,0 +1,88 @@
+package com.dkorolev.edu.java.javatestprj01.loops;
+
+import java.util.Scanner;
+
+public class TestLoop {
+    public static void main(String[] args) {
+	String mtrS = readString();
+	int[][] mtr = stringToIntArray(mtrS);
+	System.out.println(halfMtrWS(mtr));
+    }
+
+    private static String halfMtrWS(int[][] mtr) {
+	StringBuilder result = new StringBuilder();
+
+	for (int i = 0; i < mtr.length; i++) {
+	    for (int j = 0; j <= i; j++) {
+		result.append(mtr[i][j]).append(" ");
+	    }
+	    result.append("\n");
+	}
+
+	return result.toString();
+    }
+
+    private static String readString() {
+	Scanner sc = null;
+	try {
+	    sc = new Scanner(System.in);
+	    StringBuilder result = new StringBuilder();
+	    boolean isFirstLine = true;
+	    boolean continueFlag = true;
+	    while (continueFlag) {
+		if (!isFirstLine) {
+		    result.append("\n");
+		} else {
+		    isFirstLine = false;
+		}
+		String line = sc.nextLine();
+		if ("".equals(line)) {
+		    continueFlag = false;
+		} else {
+		    result.append(line);
+		}
+	    }
+
+	    return result.toString();
+	} finally {
+	    if (sc != null) {
+		sc.close();
+	    }
+	}
+    }
+
+    private static int[][] stringToIntArray(String s) {
+	String[] rows = s.split("\\n");
+	int[][] result = null;
+	for (int i = 0; i < rows.length; i++) {
+	    String row = rows[i];
+	    String[] elements = row.split(" ");
+
+	    if (result == null) {
+		result = new int[rows.length][elements.length];
+	    }
+
+	    for (int j = 0; j < elements.length; j++) {
+		result[i][j] = Integer.valueOf(elements[j]);
+	    }
+	}
+	return result;
+    }
+
+    private static String mtrToString(int[][] mtr) {
+	StringBuilder result = new StringBuilder();
+	for (int i = 0; i < mtr.length; i++) {
+	    for (int j = 0; j < mtr[i].length; j++) {
+		result.append(mtr[i][j]);
+		if (j != mtr[i].length - 1) {
+		    result.append(" ");
+		}
+	    }
+	    if (i != mtr.length - 1) {
+		result.append("\n");
+	    }
+	}
+
+	return result.toString();
+    }
+}
